7987ac473464118e1cec82483c71c4b1dfc4dba2
James Spence <Jamesaspence20@gmail.com>
2018-03-11 00:41:50 -0600

    routes for all three login types

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@
 Route::redirect('login', '/guest/login')
     ->name('login');
 
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::post('logout', 'Auth\LogoutController@logout')->name('logout');
 });
 
@@ -26,3 +26,38 @@ Route::prefix('guest')->group(function () {
         })->name('guest-home');
     });
 });
+
+Route::prefix('host')->group(function () {
+    Route::namespace('Login')->group(function () {
+        Route::get('login', 'HostLoginController@showLoginForm')->name('host-login');
+        Route::post('login', 'HostLoginController@login');
+    });
+    Route::namespace('Register')->group(function () {
+        Route::get('register', 'HostRegistrationController@showRegistrationForm')->name('host-register');
+        Route::post('register', 'HostRegistrationController@register')->name('host-register');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('home', function () {
+            return 'Home!';
+        })->name('host-home');
+    });
+});
+
+Route::prefix('admin')->group(function () {
+    Route::namespace('Login')->group(function () {
+        Route::get('login', 'AdminLoginController@showLoginForm')->name('admin-login');
+        Route::post('login', 'AdminLoginController@login');
+    });
+    Route::namespace('Register')->group(function () {
+        Route::get('register', 'AdminRegistrationController@showRegistrationForm')->name('admin-register');
+        Route::post('register', 'AdminRegistrationController@register')->name('admin-register');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('home', function () {
+            return 'Home!';
+        })->name('admin-home');
+    });
+});
+
