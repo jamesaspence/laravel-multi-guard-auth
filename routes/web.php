82ca87fbe7f59586c3f690eb2617b803308df118
James Spence <Jamesaspence20@gmail.com>
2018-03-11 00:41:38 -0600

    handling of default home route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+use App\Models\Role;
+use App\Models\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    /** @var User $user */
+    $user = Auth::user();
+
+    if (is_null($user) || $user->roles->isEmpty()) {
+        return redirect(route('guest-login'));
+    }
+
+    /** @var Role $role */
+    $role = $user->roles->first();
+
+    return redirect(route($role->name . '-home'));
+})->name('home');
